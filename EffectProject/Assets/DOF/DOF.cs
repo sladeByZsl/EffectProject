022435f597b515dfbd52190c1c2f131c5d7442f6
zhousilai <zhousilai@cookygame.com>
2019-08-20 07:55:03 +0800

    提交

---
+++ EffectProject/Assets/DOF/DOF.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class DOF : PostEffectBase
+{
+    void OnEnable()
+    {
+        GetComponent<Camera>().depthTextureMode |= DepthTextureMode.Depth;
+    }
+
+    void OnDisable()
+    {
+        GetComponent<Camera>().depthTextureMode &= ~DepthTextureMode.Depth;
+    }
+
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        if (_Material)
+        {
+            Graphics.Blit(source, destination, _Material);
+        }
+    }
+}
